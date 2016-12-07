ded9d40f2a5f374a28b8b300e7b9d911c2f33bf6
kolendos <seb@kolendos.com>
2016-12-07 10:22:14 -0600

    Functions basically - map navigation works with WASD

--- js/canvass.js
+++ js/canvass.js
@@ -1,4 +1,4 @@
-const SCALE = 1;
+const SCALE = 3;
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 var centerX = canvas.width / 2;
@@ -14,29 +14,6 @@ function drawCenterPoint(color) {
   context.fillRect(centerX, centerY, SCALE, SCALE);
 }
 
-function drawPointFromCenter(distance, direction) {
-  var pointX, pointY;
-
-  switch(direction) {
-    case FACE_NORTH:
-      pointX = centerX;
-      pointY = centerY - (distance * SCALE);
-      break;
-    case FACE_SOUTH:
-      pointX = centerX;
-      pointY = centerY + (distance * SCALE);
-      break;
-    case FACE_WEST:
-      pointX = centerX - (distance * SCALE);
-      pointY = centerY;
-      break;
-    case FACE_EAST:
-      pointX = centerX + (distance * SCALE);
-      pointY = centerY;
-      break;
-  }
-  return [pointX, pointY];
-}
 
 function drawAtPoint(x, y, color) {
   color = color ? color : "black";
--- js/drone.js
+++ js/drone.js
@@ -4,98 +4,87 @@ var drone = {
     facing: FACE_NORTH
 };
 
-var historyLength = 10;
-var rangeHistory = [];
-
-function updateCanvas() {
+function updateCanvas(pointX, pointY) {
   // blank everything
   blankCanvas();
   // make a point for the drone
   drawCenterPoint('green');
-  // draw everything from the history
-  for(var i = 0; i < rangeHistory.length; i++) {
-    var histX = rangeHistory[i].x;
-    var histY = rangeHistory[i].y;
-    drawAtPoint(histX, histY, "black");
-  }
-
-}
-
-function updateHistory(direction) {
-  var moveX, moveY;
-  switch(direction) {
-    case FACE_NORTH:
-      moveX = 1; moveY = 0; break;
-    case FACE_SOUTH:
-      moveX = -1; moveY = 0; break;
-    case FACE_WEST:
-      moveX = 1; moveY = 0; break;
-    case FACE_EAST:
-      moveX = -1; moveY = 0; break;
-  }
-  
-  // NOTE - this starts at index 1, since
-  // the first index will always be the item
-  // we just added; no change is necessary;
-  for(var i = 1; i < rangeHistory.length; i++) {
-    rangeHistory[i].x += moveX;
-    rangeHistory[i].y += moveY;
-  }
+  // draw the point in front of us
+  drawAtPoint(pointX, pointY);
 }
 
 const KEY_LEFT = 37;
 const KEY_UP = 38;
 const KEY_RIGHT = 39;
 const KEY_DOWN = 40;
-const KEY_W = 87;
+const KEY_W = 119;
 const KEY_A = 97;
-const KEY_S = 83;
-const KEY_D = 68;
+const KEY_S = 115;
+const KEY_D = 100;
 function moveDrone(event) {
+  console.log(event.keyCode);
   var currentX = drone.x;
   var currentY = drone.y;
   var direction;
   switch(event.keyCode) {
     case KEY_LEFT:
     case KEY_A:
-      direction = FACE_WEST;
+      drone.facing = FACE_WEST;
       drone.x -= 1;
       break;
     case KEY_UP:
     case KEY_W:
-      direction = FACE_NORTH;
+      drone.facing = FACE_NORTH;
       drone.y -= 1;
       break;
     case KEY_DOWN:
     case KEY_S:
-      direction = FACE_SOUTH;
+      drone.facing = FACE_SOUTH;
       drone.y += 1;
       break;
     case KEY_RIGHT:
     case KEY_D:
-      direction = FACE_EAST;
+      drone.facing = FACE_EAST;
       drone.x += 1;
       break;
     default: return;
   }
-  console.log('here');
   if(!isOnFloor(drone.x, drone.y)) {
     // we're outside the map, so reset the drone's x,y
-    console.log('in a wall?');
     drone.x = currentX;
     drone.y = currentY;
   } else {
     
     // get the wall infront of us, add it to the history 
     var distance = distanceToWall(drone.x, drone.y, drone.facing);
-    var distantPoint = drawPointFromCenter(distance, drone.facing);
-    var newHist = {x: distantPoint[0], y: distantPoint[1]};
-    rangeHistory.unshift(newHist);
-    updateHistory(direction);
-    updateCanvas();
+    var distantPoint = findPointFromCenter(distance, drone.facing);
+    updateCanvas(distantPoint[0], distantPoint[1]);
   }
 }
 
+function findPointFromCenter(distance, direction) {
+  var pointX, pointY;
+
+  switch(direction) {
+    case FACE_NORTH:
+      pointX = centerX;
+      pointY = centerY - (distance * SCALE);
+      break;
+    case FACE_SOUTH:
+      pointX = centerX;
+      pointY = centerY + (distance * SCALE);
+      break;
+    case FACE_WEST:
+      pointX = centerX - (distance * SCALE);
+      pointY = centerY;
+      break;
+    case FACE_EAST:
+      pointX = centerX + (distance * SCALE);
+      pointY = centerY;
+      break;
+  }
+  return [pointX, pointY];
+}
 function startDrone(startingX, startingY) {
   drone.x = startingX;
   drone.y = startingY;
